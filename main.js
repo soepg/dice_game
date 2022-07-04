4ef24dad7fb4a298d7cbcbdad0c2964c9d66127a
datmo <kirito250408@gmail.com>
2022-07-04 19:14:44 +0300

    first

---
+++ main.js
@@ -0,0 +1,14 @@
+let user_number=document.getElementById("user_number");
+let btn=document.getElementById("ok_btn");
+let random_div=document.getElementById("random");
+let your_number=document.getElementById("your_number")
+btn.onclick=function () {
+    let random=Math.trunc(Math.random()*10);
+    your_number.innerHTML=user_number.value;
+    random_div.innerHTML=random;
+    console.log(random);
+    if (user_number.value==random){
+        alert("ПОБЕДА!!!");
+    }
+    user_number.value="";
+}
