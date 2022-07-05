a28279476d5f1cde5acfcbeae8992c9e995003fd
datmo <kirito250408@gmail.com>
2022-07-05 20:22:10 +0300

    commit7

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ let btn=document.getElementById("ok_btn");
 let random_div=document.getElementById("random");
 let your_number=document.getElementById("your_number")
 btn.onclick=function () {
-    let random=Math.trunc(Math.floor(Math.random() * (5 - 1 + 1)) + 1);
+    min = Math.ceil(1);
+    max = Math.floor(5);
+    let random=Math.trunc(Math.floor(Math.random() * (max - min + 1)) + min);
     your_number.innerHTML=user_number.value;
     random_div.innerHTML=random;
     console.log(random);
